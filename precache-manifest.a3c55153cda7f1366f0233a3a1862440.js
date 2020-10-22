170b331727cab09acc0b8f74f6b2f66cd070eccd
Ayushi Bansal <65790730+AYUSHI-B83@users.noreply.github.com>
2020-10-22 23:57:44 +0530

    Update precache-manifest.a3c55153cda7f1366f0233a3a1862440.js

--- precache-manifest.a3c55153cda7f1366f0233a3a1862440.js
+++ precache-manifest.a3c55153cda7f1366f0233a3a1862440.js
@@ -29,30 +29,30 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "4d06c7307648f335ed8c9a5452897bb4",
-    "url": "/BIG-info/static/media/2.4d06c730.jpeg"
+    "url": "/static/media/2.4d06c730.jpeg"
   },
   {
     "revision": "e958f5c043e48dfb0eefd2f9256ff200",
-    "url": "/BIG-info/static/media/bills.e958f5c0.jpeg"
+    "url": "/static/media/bills.e958f5c0.jpeg"
   },
   {
     "revision": "36974cfdad142eb419808caecf4b5561",
-    "url": "/BIG-info/static/media/c.36974cfd.jpeg"
+    "url": "/static/media/c.36974cfd.jpeg"
   },
   {
     "revision": "bc45b82ad7b91dbff6cc850af9ae07a8",
-    "url": "/BIG-info/static/media/cccc.bc45b82a.jpg"
+    "url": "/static/media/cccc.bc45b82a.jpg"
   },
   {
     "revision": "7c2a0548226d1b04a657b9a17c7c861c",
-    "url": "/BIG-info/static/media/ccccc.7c2a0548.jpg"
+    "url": "/static/media/ccccc.7c2a0548.jpg"
   },
   {
     "revision": "4db34d5145ca118c917e219654e0a6ae",
-    "url": "/BIG-info/static/media/contactus.4db34d51.jpeg"
+    "url": "/static/media/contactus.4db34d51.jpeg"
   },
   {
     "revision": "3f7914cab9f7d1534762e1fdb9fa509e",
-    "url": "/BIG-info/static/media/paper1.3f7914ca.jpg"
+    "url": "/static/media/paper1.3f7914ca.jpg"
   }
 ]);
